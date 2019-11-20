bc091a8ec1b3bcd4c400d532268ec1d71f26b4df
Dinabandhu <beheradinabandhu50@gmail.com>
2019-11-20 22:48:27 +0530

    built base model

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ def abalone_sex():
     '''Hello world function'''
     if request.method == 'POST':
         length = request.form['length']
-        
+        diameter = request.form['diameter']
+        height = request.form['height']
+        whole_weight = request.form['whole_weight']
+        shucked_weight = request.form['shucked_weight']
+        viscera_weight = request.form['viscera_weight']
+        shell_weight = request.form['shell_weight']      
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
